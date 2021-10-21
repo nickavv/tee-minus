cf165c56b46ede7ad7a6ec7b082963c8dc68c2fe
nickavv <hello@nickvv.me>
2021-10-21 11:01:11 -0400

    Initial commit of Gatsby application

---
+++ main2/src/components/Countdown/Countdown.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+import './Countdown.css';
+
+class Countdown extends Component {
+
+    render () {
+        return (
+            <div id='countdown'>
+                TODO: Implement Countdown
+            </div>
+        )
+    }
+
+}
+
+export default Countdown;
---
+++ main2/src/pages/index.js
@@ -0,0 +1,16 @@
+import * as React from "react"
+
+import Countdown from '../components/Countdown/Countdown';
+
+import '../main.css';
+
+const IndexPage = () => {
+  return (
+    <main>
+      <title>Tee-Minus</title>
+      <Countdown />
+    </main>
+  )
+}
+
+export default IndexPage
